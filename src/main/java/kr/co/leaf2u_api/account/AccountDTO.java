aed4797df9ff29bfd75c3949a92578bcb00803dd
moonkyoungmi <cher1605@naver.com>
2025-03-04 13:53:45 +0900

    납입내역에 계좌 잔액, 금리 조회 추가

--- src/main/java/kr/co/leaf2u_api/account/AccountDTO.java
+++ src/main/java/kr/co/leaf2u_api/account/AccountDTO.java
@@ -1,21 +1,39 @@
 package kr.co.leaf2u_api.account;
 
-import lombok.Getter;
-import lombok.Setter;
+import kr.co.leaf2u_api.entity.Account;
+import lombok.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDTO {
 
-    private Long memberIdx;
-    private String accountPassword;
-    private Boolean cardYn;
-
-    private String accountNumber;
-    private BigDecimal balance;
-    private BigDecimal interestRate;
-    private BigDecimal primeRate;
+    private Long idx;  // 계좌 Idx
+    private Long memberIdx;  // 사용자 Idx
+    private Character accountStatus;  // 계좌 상태
+    private String accountNumber;  // 계좌 번호
+    private String accountPassword;  // 계좌 비밀번호
+    private BigDecimal paymentAmount;  // 납입금액
+    private BigDecimal balance;  // 잔액
+    private BigDecimal interestRate;  // 기본 금리
+    private BigDecimal primeRate;  // 우대 금리
+    private BigDecimal finalInterestRate;  // 최종 금리
+    private Character taxationYn;  // 과세 구분
+    private BigDecimal interestAmount;  // 이자
+    private LocalDateTime createDate;  // 가입일
+    private LocalDateTime endDate;  // 종료일
+    private LocalDateTime updateDate;  // 수정일
+    private LocalDateTime maturityDate;  // 만기일
+    
+    // 현재 잔액, 금리 조회용 생성자
+    public AccountDTO(BigDecimal balance, BigDecimal finalInterestRate) {
+        this.balance = balance;
+        this.finalInterestRate = finalInterestRate;
+    }
 
 }
