e843f6f79c9299404a9cb269c3d8bdb22da8a33f
sangwooklee <145044337+sangwookle@users.noreply.github.com>
2025-03-06 10:37:55 +0900

    PointController

--- src/main/java/kr/co/leaf2u_api/point/PointController.java
+++ src/main/java/kr/co/leaf2u_api/point/PointController.java
@@ -1,4 +1,14 @@
 package kr.co.leaf2u_api.point;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/point")
+@RequiredArgsConstructor
 public class PointController {
+
+
+
 }
