db84afce8519fe66510ee34e029321d0f0bdbe1d
sangwooklee <145044337+sangwookle@users.noreply.github.com>
2025-03-10 17:08:01 +0900

    point 총합

--- src/main/java/kr/co/leaf2u_api/point/PointService.java
+++ src/main/java/kr/co/leaf2u_api/point/PointService.java
@@ -2,6 +2,8 @@ package kr.co.leaf2u_api.point;
 
 import kr.co.leaf2u_api.entity.Member;
 
+import java.math.BigDecimal;
+
 public interface PointService {
     boolean checkIn(Member member);
     void addPedometerPoints(Member member, int points);
@@ -9,4 +11,5 @@ public interface PointService {
     void addQuizHintPoint(Member member);
     boolean checkQuizAnswer(String answer);
     void addQuizCorrectPoint(Member member);
+    BigDecimal getTotalPoints(Member member);
 }
