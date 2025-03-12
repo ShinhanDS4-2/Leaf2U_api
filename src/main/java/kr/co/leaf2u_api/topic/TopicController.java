bae3e87824e82c3fb709d43723c6f8b222e5698e
sangwooklee <145044337+sangwookle@users.noreply.github.com>
2025-03-12 11:45:27 +0900

    topic 수정

--- src/main/java/kr/co/leaf2u_api/topic/TopicController.java
+++ src/main/java/kr/co/leaf2u_api/topic/TopicController.java
@@ -1,13 +1,10 @@
 package kr.co.leaf2u_api.topic;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.co.leaf2u_api.entity.EcoTips;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -32,7 +29,6 @@ public class TopicController {
 
     /**
      * 뉴스 기사 api
-     * @return
      */
     @GetMapping("/news")
     public ResponseEntity<List<Map<String, Object>>> getNews() {
@@ -42,11 +38,8 @@ public class TopicController {
         return ResponseEntity.ok(response);
     }
 
-
-
     /**
      * 미세먼지
-     * @return
      */
     @GetMapping("/finedust")
     public ResponseEntity<Map<String, Object>> getFineDust() {
